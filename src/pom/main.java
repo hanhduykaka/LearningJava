eb6528685a1f1b46c68a7ca172a8758e453fd9e0
taittn <taittn@hitachiconsulting.com>
2019-07-12 15:15:15 +0700

    modified

--- src/pom/main.java
+++ src/pom/main.java
@@ -3,10 +3,56 @@
  */
 package pom;
 
+import java.util.ArrayList;
+
 /**
  * @author taittn.gh
  *
  */
+
+
+interface  People{
+	int legs = 0 ;
+	abstract  void makeSound();
+}
+
+abstract class Animal{
+	int legs = 0 ;
+	abstract  void makeSound();
+}
+class Cat extends Animal implements People{
+	public void makeSound() {
+		System.out.print("Meow\n");
+	}
+}
+
+class Machine{
+	public void start() {
+		System.out.print("start\n");
+	}
+}
+
+// Thread // extends Thread
+
+class Loader extends Thread{
+	public void run() {
+		System.out.println("hes loo");
+	}
+}
+
+//
+
+// Another Thread create // implements Runnable
+
+class Loader2 implements Runnable{
+	public void run() {
+		System.out.println("hes loo 2");
+	}
+}
+
+
+
+
 public class main {
 
 	/**
@@ -33,6 +79,27 @@ public class main {
 			}
 		};
 		m.start();
+		
+		
+		// Thread
+		Loader obj = new Loader();
+		try {
+			obj.sleep(2000);
+			obj.start();
+		} catch (InterruptedException e) {		
+			e.printStackTrace();
+		}
+		// end Thread
+		
+		// Thread Runnale
+		Thread t = new Thread(new Loader2());
+		t.start();
+		
+		
+		// Array list
+		ArrayList colors = new ArrayList<>();
+		 colors.add("Red");
+			System.out.print(colors);
 	}	
 	
 	public static int COUNT =0 ; 
@@ -40,25 +107,6 @@ public class main {
 }
 
 
-interface  People{
-	int legs = 0 ;
-	abstract  void makeSound();
-}
 
-abstract class Animal{
-	int legs = 0 ;
-	abstract  void makeSound();
-}
-class Cat extends Animal implements People{
-	public void makeSound() {
-		System.out.print("Meow\n");
-	}
-}
-
-class Machine{
-	public void start() {
-		System.out.print("start\n");
-	}
-}
 
 
