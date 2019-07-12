85d2ecb644bb351b6d19dd0298d1468119a92801
taittn <taittn@hitachiconsulting.com>
2019-07-12 16:54:25 +0700

    add more subject

--- src/pom/main.java
+++ src/pom/main.java
@@ -3,30 +3,37 @@
  */
 package pom;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Scanner;
 
 /**
  * @author taittn.gh
  *
  */
 
+interface People {
+	int legs = 0;
 
-interface  People{
-	int legs = 0 ;
-	abstract  void makeSound();
+	abstract void makeSound();
 }
 
-abstract class Animal{
-	int legs = 0 ;
-	abstract  void makeSound();
+abstract class Animal {
+	int legs = 0;
+
+	abstract void makeSound();
 }
-class Cat extends Animal implements People{
+
+class Cat extends Animal implements People {
 	public void makeSound() {
 		System.out.print("Meow\n");
 	}
 }
 
-class Machine{
+class Machine {
 	public void start() {
 		System.out.print("start\n");
 	}
@@ -34,7 +41,7 @@ class Machine{
 
 // Thread // extends Thread
 
-class Loader extends Thread{
+class Loader extends Thread {
 	public void run() {
 		System.out.println("hes loo");
 	}
@@ -44,14 +51,11 @@ class Loader extends Thread{
 
 // Another Thread create // implements Runnable
 
-class Loader2 implements Runnable{
+class Loader2 implements Runnable {
 	public void run() {
 		System.out.println("hes loo 2");
 	}
 }
-
-
-
 
 public class main {
 
@@ -60,7 +64,7 @@ public class main {
 	 */
 	public main() {
 		// TODO Auto-generated constructor stub
-	
+
 	}
 
 	/**
@@ -69,44 +73,83 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		COUNT++;
-		System.out.print(COUNT+"\n");
+		System.out.print(COUNT + "\n");
 		Cat c1 = new Cat();
 		c1.makeSound();
-		
+
 		Machine m = new Machine() {
-			@Override public void start() {
+			@Override
+			public void start() {
 				System.out.print("whoo\n");
 			}
 		};
 		m.start();
-		
-		
+
 		// Thread
 		Loader obj = new Loader();
 		try {
 			obj.sleep(2000);
 			obj.start();
-		} catch (InterruptedException e) {		
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		// end Thread
-		
+
 		// Thread Runnale
 		Thread t = new Thread(new Loader2());
 		t.start();
-		
-		
+
 		// Array list
 		ArrayList colors = new ArrayList<>();
-		 colors.add("Red");
-			System.out.print(colors);
-	}	
-	
-	public static int COUNT =0 ; 
+		colors.add("Red");
+		System.out.print(colors + "\n");
+		System.out.print(colors.get(0) + "\n");
+		System.out.print(colors.size() + "\n");
+		System.out.print(colors.contains("Red") + "\n");
+		colors.add("Blue");
+		colors.add("Alue");
+		System.out.println(colors.size());
+		System.out.println(colors);
+		// sorting
+		Collections.sort(colors);
+		System.out.println(colors);
+
+		// Iterator
+		Iterator<String> it = colors.iterator();
+		String value = it.next();
+		value = it.next();
+		System.out.println(value);
+		boolean hasNextValue = it.hasNext();
+		System.out.println(hasNextValue);
+
+		it.remove();
+
+		boolean hasNextValueAfter = it.hasNext();
+		System.out.println(value);
+		//		
+		
+		// File
+		File x = new File("D:\\eula.1028.txt");
+		if (x.exists()) {
+			System.out.println(x.getName() + " exists!");
+			try {
+				  Scanner sc = new Scanner (x);
+				  while (sc.hasNext()) {
+				      String a = sc .next();
+				      String b = sc.next ();
+				      System.out.println(a + " " + b);
+				  } 
+				  sc.close ();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("The file does not exist");
+		}
+
+	}
+
+	public static int COUNT = 0;
 
 }
-
-
-
-
-
